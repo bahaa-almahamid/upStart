afdd4fdad79bfba72c4e284e280f694dbc408eb2
bahaa-almahamid <bahaamahamid@gmail.com>
2018-08-12 20:37:54 +0200

    First base

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Form\UserFormType;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+class RegistrationController extends AbstractController
+{
+    
+    public function registerUser(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+        return $this->render(
+            'registration/register.html.twig',
+            array('form' => $form->createView())
+        );
+
+    }
+}
