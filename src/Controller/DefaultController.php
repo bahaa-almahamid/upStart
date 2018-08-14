db5661736b7443086a45263ea128db485b9d0368
JoaoOnofre <joao_nuno_1986@hotmail.com>
2018-08-14 14:49:44 +0200

    Login Redirection TEST

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-
-
+use App\Entity\Role;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 
 class DefaultController extends Controller
@@ -13,4 +13,30 @@ class DefaultController extends Controller
     {
         return $this->render('default/homepage.html.twig'); //
     }
+
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+            // get the login error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
+        
+            // last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
+        
+            return $this->render('default/login.html.twig', array(
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
+    // public function downloadDocument(Document $document)
+    // {
+    //     $filename = sprintf(
+    //         '%s/%s',
+    //         $document->getPath(),
+    //         $document->getName()
+    //     );
+
+    //     return new BinaryFileResponse($filename);
+    // }
 }
